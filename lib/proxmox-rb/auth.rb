beef4c3cf77b4f4299e87c150492ad7923542264
Vladislav Moskovets <github@vlad.org.ua>
2015-12-09 03:42:39 +0200

    added options for SSL_VERIFY param

--- lib/proxmox-rb/auth.rb
+++ lib/proxmox-rb/auth.rb
@@ -1,15 +1,17 @@
 module ProxmoxRb
   class Auth
     Ticket = Struct.new(:host, :ticket, :csrf)
-    def self.get_ticket(host, username, password)
+    def self.get_ticket(host, username, password, options = {verify_ssl: OpenSSL::SSL::VERIFY_PEER})
       retry_count = 0
       auth = nil
       begin
-        auth = JSON.parse(RestClient.post 'https://' + host + ':8006/api2/json/access/ticket',
-                          { username: username, password: password })
+        resource = RestClient::Resource.new("https://#{host}:8006/api2/json/access/ticket", options)
+        json = resource.post username: username, password: password
+        auth = JSON.parse json
       rescue => e
-        sleep_count += 1
-        retry if sleep_count < 5
+        retry_count += 1
+        retry if retry_count < 5
+        raise e
       end
       ticket = auth['data']['ticket']
       csrf = auth['data']['CSRFPreventionToken']
